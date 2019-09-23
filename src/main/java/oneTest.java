333348bfbf07fe0da52a5589f1249d8608a97819
chenhao <chen_chonghao@126.com>
2019-09-23 22:39:51 +0800

    提交集合

---
+++ src/main/java/oneTest.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class oneTest {
+    public static void main(String[] args) {
+
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add("111");
+        objects.add("222");
+        objects.add("333");
+        objects.add("444");
+        objects.add("555");
+
+    }
+}
